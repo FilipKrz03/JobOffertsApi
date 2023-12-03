103be0a45c4c69448e1f87a38fe79a4e643cee1a
filip <filipos2003@gmail.com>
2023-12-03 17:21:23 +0100

    Add project files.

---
+++ WebScrapper/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+IHost _host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+
+    }).Build();
