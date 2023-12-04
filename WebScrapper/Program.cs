0644040470c5392a048662664c87c50e2d290626
Filip Krzemień <filipos2003@gmail.com>
2023-12-04 02:12:45 +0100

    feat: added dependency injection in web scrapper service and basic logic of PracujPlScrapper

--- WebScrapper/Program.cs
+++ WebScrapper/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebScrapperService.Interfaces;
+using WebScrapperService.Services;
 
 Console.WriteLine("Hello, World!");
 
 IHost _host = Host.CreateDefaultBuilder()
     .ConfigureServices(services =>
     {
-
+        services.AddSingleton<IScrapperService, PracujPlScrapper>();
     }).Build();
+
+var app = _host.Services.GetRequiredService<IScrapperService>();
+
+app.ScrapOfferts();
---
+++ WebScrapper/Services/PracujPlScrapper.cs
@@ -0,0 +1,87 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebScrapperService.Interfaces;
+
+namespace WebScrapperService.Services
+{
+    public class PracujPlScrapper : IScrapperService
+    {
+        private int PageNumber { get; set; } = 1;  
+        private const string BaseUrl = "https://it.pracuj.pl/praca?pn=";
+
+        private string FullUrl => $"{BaseUrl}{PageNumber}";
+
+        private readonly ChromeDriver _driver;
+
+        public PracujPlScrapper()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void ScrapOfferts()
+        {
+            bool isInit = true;
+
+            while(true)
+            {
+                _driver.Navigate().GoToUrl(FullUrl);
+
+                if(isInit)
+                {
+                    _driver.FindElement(By.CssSelector("[data-test='button-submitCookie']")).Click();
+                    isInit = false;
+                }
+
+                var jobElements = _driver.FindElements(By.ClassName("c1fljezf")).ToList();
+
+                if (jobElements.Count == 0) break;
+
+                IEnumerable<string> jobLinks = GetJobLinks(jobElements);
+
+                foreach (var jobLink in jobLinks)
+                {
+                    GetJobDetail(jobLink);
+                }
+
+                PageNumber++;
+            }           
+        }
+
+        private void GetJobDetail(string jobPageLink)
+        {
+            _driver.Navigate().GoToUrl(jobPageLink);
+
+            try
+            {
+                var jobTitle = _driver.FindElement(By.TagName("h1")).Text;
+                var company = _driver.FindElement(By.TagName("h2")).Text;
+
+                Console.WriteLine($"Job title {jobTitle} , comapny {company}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            } 
+        }
+
+        private IEnumerable<string> GetJobLinks(List<IWebElement> elements)
+        {
+            ICollection<string> links = new List<string>();
+
+            foreach (var element in elements)
+            {
+                var href = element.FindElement(By.ClassName("core_n194fgoq")).GetAttribute("href");
+
+                links.Add(href);
+            }
+
+            return links;
+        }
+    }
+}
