88f7f49e132eb8e633c110a8b069ad7e4809eec8
Filip Krzemień <filipos2003@gmail.com>
2023-12-06 01:28:26 +0100

    feat: added JobOffer record to Dto folder

---
+++ WebScrapper/Dto/JobOffer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapperService.Dto
+{
+    public record JobOffer(string JobTitle , string Company , string Localization , 
+        string WorkMode , string Seniority , IEnumerable<string> RequiredTechnologies);
+    
+}
