420c6ba95c1819da617f9b4ac3722a4f8fec0fb4
Filip Krzemień <filipos2003@gmail.com>
2023-12-07 13:17:58 +0100

    feat: added scrapper event manager service and regisgter it in DI pipeline

--- JobOfferService/Program.cs
+++ JobOfferService/Program.cs
@@ -1,5 +1,6 @@
 using JobOfferService.Interfaces;
 using JobOfferService.Producer;
+using JobOfferService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IScrapperMessageProducer, ScrapperMessageProducer>();
+builder.Services.AddHostedService<ScrapperEventManagerService>();
 
 
 var app = builder.Build();
---
+++ JobOfferService/Services/ScrapperEventManagerService.cs
@@ -0,0 +1,25 @@
+﻿using JobOfferService.Interfaces;
+
+namespace JobOfferService.Services
+{
+    public class ScrapperEventManagerService : BackgroundService
+    {
+
+        IScrapperMessageProducer _scrapperMessageProducer;
+
+        public ScrapperEventManagerService(IScrapperMessageProducer scrapperMessageProducer)
+        {
+            _scrapperMessageProducer = scrapperMessageProducer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                _scrapperMessageProducer.SendCreateOffersMessage();
+
+                await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+        }
+    }
+}
