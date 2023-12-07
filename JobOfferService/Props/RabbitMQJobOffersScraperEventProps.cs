4bdf0e1b43a97492636fd4cb145ff2cf1a3e8251
Filip Krzemień <filipos2003@gmail.com>
2023-12-07 02:37:55 +0100

    feat: added Properties that rabbit mq scrapper will need

---
+++ JobOfferService/Props/RabbitMQJobOffersScraperEventProps.cs
@@ -0,0 +1,11 @@
+﻿namespace JobOfferService.Props
+{
+    public static class RabbitMQJobOffersScraperEventProps
+    {
+        public const string JOB_OFFERS_SCRAPER_EXCHANGE = "job_offer_scrapper_events";
+
+        public const string OFFERS_CREATE_QUEUE = "offers.create";
+
+        public const string OFFERS_CREATE_ROUTING_KEY = "offers.create";
+    }
+}
