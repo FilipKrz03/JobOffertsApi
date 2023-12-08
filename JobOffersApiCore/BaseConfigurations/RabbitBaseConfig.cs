8b098c6333e49de30aa54cd8693debc844c6f7db
Filip Krzemień <filipos2003@gmail.com>
2023-12-08 11:50:01 +0100

    feat: added JobOffersApiCore project that will have reusable logic like rabbitConfig etc. '

---
+++ JobOffersApiCore/BaseConfigurations/RabbitBaseConfig.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace JobOffersApiCore.BaseConfigurations
+{
+    public abstract class RabbitBaseConfig
+    {
+        protected readonly IConnection _connection;
+        protected readonly IModel _chanel;
+
+        public RabbitBaseConfig(string connectionUri , string clinetProvidedName)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(connectionUri),
+                ClientProvidedName = clinetProvidedName
+            };
+
+            _connection = factory.CreateConnection();
+            _chanel = _connection.CreateModel();
+        }
+    }
+}
