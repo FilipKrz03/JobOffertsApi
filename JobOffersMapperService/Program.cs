1b35f51caa642f62174f4d9c35eff2a4e41fdd15
Filip Krzemień <filipos2003@gmail.com>
2023-12-08 14:38:08 +0100

    feat: base conifg of mapper service

--- JobOffersMapperService/Program.cs
+++ JobOffersMapperService/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+IHost _host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(builder =>
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true)
+        .AddEnvironmentVariables();
+
+        Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Build())
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+    })
+    .ConfigureServices(services =>
+    {
+    
+    })
+    .UseSerilog()
+    .Build();
+
