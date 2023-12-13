b183074938e0500bacf5c89ef22a8208dea148f2
Filip Krzemień <filipos2003@gmail.com>
2023-12-13 14:28:16 +0100

    feat: added entities in job offers service that are in many to many realtion

---
+++ JobOfferService/Entities/JobOffer.cs
@@ -0,0 +1,27 @@
+﻿using JobOffersApiCore.BaseObjects;
+using JobOffersApiCore.Enum;
+
+namespace JobOffersService.Entities
+{
+    public class JobOffer : BaseEntity
+    {
+        
+        public string OfferTitle { get; set; }  
+        public string OfferCompany {  get; set; }   
+        public string Localization {  get; set; }
+        public string WorkMode {  get; set; }   
+        public Seniority Seniority { get; set; }
+
+        public List<Technology> Technologies { get; set; } = new();
+
+        public JobOffer(string offerTitle , string offerCompany , string localization , 
+            string workMode , Seniority seniority)
+        {
+            OfferTitle = offerTitle;
+            OfferCompany = offerCompany;
+            Localization = localization;
+            WorkMode = workMode;
+            Seniority = seniority;
+        }
+    }
+}
---
+++ JobOfferService/Entities/Technology.cs
@@ -0,0 +1,17 @@
+﻿using JobOffersApiCore.BaseObjects;
+
+namespace JobOffersService.Entities
+{
+    // Many to many realationship with JobOffer Entitie 
+    public class Technology : BaseEntity
+    {
+        public string TechnologyName {  get; set; }
+
+        public List<JobOffer> JobOffers { get; set; } = new();
+
+        public Technology(string technologyName)
+        {
+            TechnologyName = technologyName;
+        }
+    }
+}
