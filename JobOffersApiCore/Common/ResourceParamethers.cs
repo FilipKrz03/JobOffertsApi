63995fa20001d056ba31cd4b0a4bcbe349d036e3
Filip Krzemień <filipos2003@gmail.com>
2023-12-17 14:20:59 +0100

    feat: added resource paramethers

---
+++ JobOffersApiCore/Common/ResourceParamethers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOffersApiCore.Common
+{
+    public class ResourceParamethers
+    {
+        public int PageSize { get; set; } = 30;
+
+        public int PageNumber {  get; set; }  = 1;  
+
+    }
+}
