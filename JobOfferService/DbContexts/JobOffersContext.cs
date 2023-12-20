1123c3c669dec48eccaf0555565116561266880d
Filip Krzemień <filipos2003@gmail.com>
2023-12-20 15:31:52 +0100

    feat: full docker support added

--- JobOfferService/DbContexts/JobOffersContext.cs
+++ JobOfferService/DbContexts/JobOffersContext.cs
@@ -1,11 +1,29 @@
 ﻿using JobOffersService.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace JobOffersService.DbContexts
 {
     public class JobOffersContext : DbContext
     {
-        public JobOffersContext(DbContextOptions<JobOffersContext> options) : base(options) { }
+        public JobOffersContext(DbContextOptions<JobOffersContext> options, ILogger<JobOffersContext> logger) : base(options)
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Exception occured in job offers context {ex}", ex);
+            }
+        }
 
         public DbSet<Technology> Technologies { get; set; } = null!;
 
--- JobOffersMapperService/DbContexts/OffersBaseContext.cs
+++ JobOffersMapperService/DbContexts/OffersBaseContext.cs
@@ -1,5 +1,8 @@
 ﻿using JobOffersMapperService.Entites;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +13,23 @@ namespace JobOffersMapperService.DbContexts
 {
     public class OffersBaseContext : DbContext
     {
-        public OffersBaseContext(DbContextOptions<OffersBaseContext> options):base(options) { }
+        public OffersBaseContext(DbContextOptions<OffersBaseContext> options , ILogger<OffersBaseContext> logger):base(options)
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Exception occured in job offers context {ex}", ex);
+            }
+        }
 
         public DbSet<JobOfferBase> BaseJobOffer { get; set; } = null!;
 
