a8926e1bf60eb60e2dd9727392c98fe8635cbda0
Filip Krzemień <filipos2003@gmail.com>
2023-12-21 03:45:17 +0100

    refactor: minior refactor

--- JobOffersMapperService/Profiles/JobOfferProcessedProfile.cs
+++ JobOffersMapperService/Profiles/JobOfferProcessedProfile.cs
@@ -14,66 +14,7 @@ namespace JobOffersMapperService.Profiles
         public JobOfferProcessedProfile()
         {
             CreateMap<JobOfferRaw, JobOfferProcessed>()
-                .ConvertUsing<JobOfferConverter>();
+                .ConvertUsing<JobOfferRawConverter>();
         }
-    }
-
-    public class JobOfferConverter : ITypeConverter<JobOfferRaw, JobOfferProcessed>
-    {
-        public JobOfferProcessed Convert(JobOfferRaw source, JobOfferProcessed destination, ResolutionContext context)
-        {
-            var seniority = ConvertSeniority(source.Seniority);
-            (int? earningsFrom, int? eariningsTo) = GetEarnings(source.salaryString);
-
-            return new
-                (source.OfferTitle, source.OfferCompany, source.Localization,
-                source.WorkMode, source.RequiredTechnologies, source.OfferLink, seniority, earningsFrom, eariningsTo);
-        }
-
-
-        private (int? earningsFrom, int? earningsTo) GetEarnings(string? earningsString)
-        {
-
-            if (earningsString == null) return (null, null);
-
-            char enDash = (char)0x2013;
-            string[] splitEarningStringArray = earningsString.Split(new char[] { enDash, '-' });
-
-            try
-            {
-                int? earningsFrom = TryParseInt(splitEarningStringArray[0].Replace(" ", ""));
-                int? earningsTo = TryParseInt(splitEarningStringArray[1].Replace(" ", "").Replace("zł", ""));
-                return (earningsFrom, earningsTo);
-            }
-            catch
-            {
-                return(null, null);
-            }
-        }
-
-        private int? TryParseInt(string source)
-        {
-            int? value;
-            int score;
-
-            if (int.TryParse(source, out score))
-            {
-                value = score;
-            }
-            else
-            {
-                value = null;
-            }
-
-            return value;
-        }
-
-        private Seniority ConvertSeniority(string source) => source.ToLower() switch
-        {
-            _ when source.Contains("junior") => Seniority.Junior,
-            _ when source.Contains("mid") => Seniority.Mid,
-            _ when source.Contains("senior") => Seniority.Senior,
-            _ => Seniority.Unknown
-        };
     }
 }
