e7fd63e14e01c84c53b63a983e05822cffe5503a
Filip Krzemień <filipos2003@gmail.com>
2023-12-31 03:25:39 +0100

    tests: filter tests added

---
+++ JobOfferServiceTests/Filters/ApiKeyAuthorizationFilterAtributteTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using JobOffersService.Filters;
+using FluentAssertions;
+
+namespace JobOfferServiceTests.Filters
+{
+    public class ApiKeyAuthorizationFilterAtributteTests
+    {
+
+        private readonly AuthorizationFilterContext _context;
+        private readonly ApiKeyAuthorizationFilterAtributte _filter;
+
+        public ApiKeyAuthorizationFilterAtributteTests()
+        {
+            _context = new(
+                new ActionContext(new DefaultHttpContext(), new RouteData(), new ActionDescriptor()),
+                Array.Empty<IFilterMetadata>()
+                );
+            _filter = new();
+        }
+
+        [Fact]
+        public void Filter_Should_ReturnUnautorizedResult_WhenApiKeyNotProvided()
+        {
+           _filter.OnAuthorization(_context);
+
+            _context.Result.Should()
+              .BeOfType<UnauthorizedResult>();
+        }
+
+        [Fact]
+        public void Filter_Should_ReturnUnauthorizedResult_WhenInvalidApiKeyProvided()
+        {
+            Environment.SetEnvironmentVariable("ApiKey", "VALID_API_KEY");
+
+            _context.HttpContext.Request.Headers["ApiKey"] = "INVALID_API_KEY";
+
+            _filter.OnAuthorization(_context);
+
+            _context.Result.Should()
+               .BeOfType<UnauthorizedResult>();
+        }
+
+        [Fact]
+        public void Filter_ShouldNot_ReturnUnautorizedResult_WhenValidApiKeyProvided()
+        {
+            string validApiKey = "VALID_API_KEY";
+
+            Environment.SetEnvironmentVariable("ApiKey", validApiKey);
+
+            _context.HttpContext.Request.Headers["ApiKey"] = validApiKey;
+
+            _filter.OnAuthorization(_context);
+
+            _context.Result.Should()
+                .BeNull();
+        }
+    }
+}
