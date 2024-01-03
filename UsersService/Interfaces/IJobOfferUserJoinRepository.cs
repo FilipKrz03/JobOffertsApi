def5085ef2eb3d85cb563437b000c083c02b2f28
Filip Krzemień <filipos2003@gmail.com>
2024-01-03 12:21:25 +0100

    feat: added methods to job offer user join repositrory

--- UsersService/Interfaces/IJobOfferUserJoinRepository.cs
+++ UsersService/Interfaces/IJobOfferUserJoinRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace UsersService.Interfaces
+﻿using UsersService.Entities;
+
+namespace UsersService.Interfaces
 {
     public interface IJobOfferUserJoinRepository
     {
         Task<bool> UserJobOfferExist(Guid userId, Guid jobId);
+        Task<JobOfferUser?> GetUserJobOffer(Guid userId, Guid jobId);
+        void RemoveUserJobOffer(JobOfferUser entity);
+        Task SaveChangesAsync();
     }
 }
--- UsersService/Repository/JobOfferUserJoinRepository.cs
+++ UsersService/Repository/JobOfferUserJoinRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UsersService.DbContexts;
+using UsersService.Entities;
 using UsersService.Interfaces;
 
 namespace UsersService.Repository
@@ -20,6 +21,23 @@ namespace UsersService.Repository
             return await _context.JobOfferUsers
                 .Where(j => j.JobOfferId == jobId && j.UserId == userId)
                 .AnyAsync();
+        }
+
+        public async Task<JobOfferUser?> GetUserJobOffer(Guid userId , Guid jobId)
+        {
+            return await _context.JobOfferUsers
+                .Where(e => e.UserId == userId && e.JobOfferId == jobId)
+                .FirstOrDefaultAsync();
+        }
+
+        public void RemoveUserJobOffer(JobOfferUser entity)
+        {
+            _context.JobOfferUsers.Remove(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();  
         }
     }
 }
