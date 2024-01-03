a571de61549a62ac7f498aaec0d4c5fa3f0d75fb
Filip Krzemień <filipos2003@gmail.com>
2024-01-03 14:57:20 +0100

    feat: added paged list converter

---
+++ JobOffersApiCore/Common/PagedListConverter.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOffersApiCore.Common
+{
+    public class PagedListConverter<TSource, TDestination> :
+        ITypeConverter<PagedList<TSource>, PagedList<TDestination>>
+        where TSource : class
+        where TDestination : class
+    {
+        public PagedList<TDestination> Convert
+            (PagedList<TSource> source, PagedList<TDestination> destination, ResolutionContext context)
+        {
+            var collection =
+                context.Mapper.Map<List<TSource>, List<TDestination>>(source);
+
+            return new
+                PagedList<TDestination>(collection, source.PageNumber, source.PageSize, source.TotalCount);
+        }
+    }
+}
