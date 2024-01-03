bfe5145917840ae9939d52d759f0e9003f1c7696
Filip Krzemień <filipos2003@gmail.com>
2024-01-03 04:30:12 +0100

    refactor: small refactor to feet new db strategy

--- JobOfferService/DbContexts/JobOffersContext.cs
+++ JobOfferService/DbContexts/JobOffersContext.cs
@@ -7,24 +7,8 @@ namespace JobOffersService.DbContexts
 {
     public class JobOffersContext : DbContext
     {
-        public JobOffersContext(DbContextOptions<JobOffersContext> options, ILogger<JobOffersContext> logger) : base(options)
-        {
-            try
-            {
-                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
-
-                if (databaseCreator != null)
-                {
-                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
-                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError("Exception occured in job offers context {ex}", ex);
-            }
-        }
-
+        public JobOffersContext(DbContextOptions<JobOffersContext> options) : base(options) { }
+       
         public DbSet<Technology> Technologies { get; set; } = null!;
 
         public DbSet<JobOffer> JobOffers { get; set; } = null!;
