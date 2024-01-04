6c02796043646de49cd8a49d011f86d712822352
Filip Krzemień <filipos2003@gmail.com>
2024-01-04 03:03:29 +0100

    test: modified profles tests

---
+++ UsersServiceTests/Profiles/ProfileTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersService.Profiles;
+
+namespace UsersServiceTests.Profiles
+{
+    public class ProfileTests
+    {
+        [Fact]
+        public void MapperConfiguration_Should_BeValid()
+        {
+            // All profiles tested because they are depending on each other
+
+            var cfg = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<UserProfile>();
+                cfg.AddProfile<JobOfferProfile>();
+                cfg.AddProfile<TechnologyProfile>();
+                cfg.AddProfile<PagedListProfile>();
+            });
+
+            cfg.AssertConfigurationIsValid();
+        }
+    }
+}
