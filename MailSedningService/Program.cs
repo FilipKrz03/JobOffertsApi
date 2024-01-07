93a37f38bd9a5a58884cb19a6ad4a387261b6f51
Filip Krzemień <filipos2003@gmail.com>
2024-01-07 16:56:38 +0100

    feat: base look of MailSendingService

---
+++ MailSedningService/Program.cs
@@ -0,0 +1,26 @@
+﻿using MailSedningService.Interfaces;
+using MailSedningService.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+IHost _host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(builder =>
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appsettings.json", false, true)
+         .AddEnvironmentVariables();
+
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddTransient<ISendMailToUsersGroupWithRecommendedOfferService, SendMailToUsersGroupWithRecommendedOfferService>();
+    })
+    .Build();
+
+var mailSender = _host.Services.GetRequiredService<ISendMailToUsersGroupWithRecommendedOfferService>();
+
+mailSender.SendMail();
+
+
---
+++ MailSedningService/Services/SendMailToUsersGroupWithRecommendedOfferService.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using MailSedningService.Interfaces;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSedningService.Services
+{
+    public class SendMailToUsersGroupWithRecommendedOfferService : ISendMailToUsersGroupWithRecommendedOfferService
+    {
+        public void SendMail()
+        {
+            var sendingEmail = Environment.GetEnvironmentVariable("sendingEmail");
+            var smtpServer = Environment.GetEnvironmentVariable("smtpServer");
+            var smtpPort = int.Parse(Environment.GetEnvironmentVariable("smtpPort")!);
+            var smtpLogin = Environment.GetEnvironmentVariable("smtpLogin");
+            var smtpPassword = Environment.GetEnvironmentVariable("smtpPassword");
+
+            var email = new MimeMessage();
+
+            string body = "Test";
+
+            email.From.Add(MailboxAddress.Parse(sendingEmail));
+            email.To.Add(MailboxAddress.Parse("filipos2003@gmail.com"));
+            email.Subject = "Test";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+
+            smtp.Connect(smtpServer, smtpPort, true);
+            smtp.Authenticate(smtpLogin, smtpPassword);
+            smtp.Send(email);
+
+            smtp.Disconnect(true);
+        }
+    }
+}
