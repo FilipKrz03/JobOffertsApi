cac30ad8e1476a5bffdded534576ff26608fa6d6
Filip Krzemień <filipos2003@gmail.com>
2024-01-07 17:22:30 +0100

    feat: improved analyzer logic

--- UsersService/Services/UserAnalyzeService.cs
+++ UsersService/Services/UserAnalyzeService.cs
@@ -1,5 +1,6 @@
 ﻿using JobOffersApiCore.Interfaces;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using UsersService.Entities;
 using UsersService.Interfaces.RepositoriesInterfaces;
 using UsersService.Interfaces.ServicesInterfaces;
@@ -45,10 +46,12 @@ namespace UsersService.Services
 
                 var userEmailsList = group.Select(e => e.Email).ToList();
 
+                var mailContent = CreateEmailContent(matchingOffers);
+
                 var messageObject = new
                 {
                     userEmailsList,
-                    matchingOffers
+                    mailContent
                 };
 
                 _sendEmailToUsersGruopWithRecommendedOffersMessageProducer
@@ -58,6 +61,36 @@ namespace UsersService.Services
                     messageObject
                     );
             }
+        }
+
+        private string CreateEmailContent(IEnumerable<JobOffer> jobOffers)
+        {
+            StringBuilder htmlContent = new();
+
+            htmlContent.AppendLine("<!DOCTYPE html>");
+            htmlContent.AppendLine("<html lang=\"en\">");
+            htmlContent.AppendLine("<head>");
+            htmlContent.AppendLine("<meta charset=\"UTF-8\">");
+            htmlContent.AppendLine("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            htmlContent.AppendLine("<title>Moja Strona</title>");
+            htmlContent.AppendLine("</head>");
+            htmlContent.AppendLine("<body>");
+
+            htmlContent.AppendLine
+                ("<h1>Hi! Especially for you, we have delivered some offers based on your preferences that might interest you </h1>");
+
+            foreach (var jobOffer in jobOffers)
+            {
+                htmlContent.AppendLine($"<a href='{jobOffer.OfferLink}'>");
+                htmlContent.AppendLine($"<h3>{jobOffer.OfferTitle}</h3>");
+                htmlContent.AppendLine($"<p> {jobOffer.OfferCompany}</p>");
+                htmlContent.AppendLine("</a>");
+            }
+
+            htmlContent.AppendLine("</body>");
+            htmlContent.AppendLine("</html>");
+
+            return htmlContent.ToString();
         }
     }
 }
