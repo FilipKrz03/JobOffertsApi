e04ff369d453e0db888e7af7c3edc7463a1dde8b
Filip Krzemień <filipos2003@gmail.com>
2024-01-08 22:15:51 +0100

    feat: added offer check if outdated consumer

---
+++ WebScrapper/Consumer/OfferCheckIfOutdatedEventConsumer.cs
@@ -0,0 +1,34 @@
+﻿using JobOffersApiCore.BaseObjects;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WebScrapperService.Props.RabbitMQJobEventProps;
+
+namespace WebScrapperService.Consumer
+{
+    internal sealed class OfferCheckIfOutdatedEventConsumer : RabbitMqBaseConsumer
+    {
+        public OfferCheckIfOutdatedEventConsumer(ILogger<OfferCheckIfOutdatedEventConsumer> logger) :
+            base(
+                Environment.GetEnvironmentVariable("RabbitConnectionUri")!,
+                JOB_CHECK_IF_OUTDATED_CLIENT_PROVIDED_NAME,
+                logger,
+                JOB_CHECK_IF_OUTDATED_QUEUE
+                )
+        {
+            DeclareQueueAndExchange(
+                JOB_CHECK_IF_OUTDATED_QUEUE,
+                JOB_SCRAPPER_EVENTS_EXCHANGE,
+                JOB_CHECK_IF_OUTDATED_ROUTING_KEY
+                );
+        }
+
+        protected override void ProccesMessage(string message)
+        {
+            
+        }
+    }
+}
