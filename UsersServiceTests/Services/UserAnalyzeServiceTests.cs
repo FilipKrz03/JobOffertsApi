fbe2c75d049dc71e6b8a3876cdada6fc50d14612
Filip Krzemień <filipos2003@gmail.com>
2024-01-11 02:10:58 +0100

    test: user services tests

---
+++ UsersServiceTests/Services/UserAnalyzeServiceTests.cs
@@ -0,0 +1,114 @@
+﻿using Bogus;
+using JobOffersApiCore.Enum;
+using JobOffersApiCore.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersService.Dto;
+using UsersService.Entities;
+using UsersService.Interfaces.RepositoriesInterfaces;
+using UsersService.Interfaces.ServicesInterfaces;
+using UsersService.Services;
+
+namespace UsersServiceTests.Services
+{
+    public class UserAnalyzeServiceTests
+    {
+
+        private readonly Mock<IJobOfferRepository> _jobOfferRepositoryMock;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IRabbitMessageProducer> _rabbitMessageProducerMock;
+        private readonly Mock<IMailContentCreatorService> _mailContentCreatorServiceMock;
+
+        private readonly UserAnalyzeService _userAnalyzeService;
+
+        public UserAnalyzeServiceTests()
+        {
+            _jobOfferRepositoryMock = new();
+            _userRepositoryMock = new();
+            _rabbitMessageProducerMock = new();
+            _mailContentCreatorServiceMock = new();
+
+            _userAnalyzeService = new(
+                _jobOfferRepositoryMock.Object,
+                _userRepositoryMock.Object,
+                _rabbitMessageProducerMock.Object,
+                _mailContentCreatorServiceMock.Object
+                );
+        }
+
+        [Fact]
+        public async Task Service_LetUsersKnowAboutNewMatchingOffers_ShouldNot_SendAnyMessages_WhenAllUsersIsEmptyList()
+        {
+            _userRepositoryMock.Setup(x =>
+              x.GetAllUsersWithEmailSeniorityAndTechnologiesAsync())
+                .ReturnsAsync(Enumerable.Empty<UserWithEmailSeniorityAndTechnolgiesDto>());
+
+            await _userAnalyzeService.LetUsersKnowAboutNewMatchingOffers();
+
+            _rabbitMessageProducerMock.Verify(x =>
+              x.SendMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Service_LetUsersKnowAboutNewMatchingOffers_Should_SendOneMessage_WhenOnlyUsersWithoutSubscribedTechnologiesExists()
+       
+        {
+            var usersWithNoSubscribedTechFake = new Faker<UserWithEmailSeniorityAndTechnolgiesDto>()
+                 .CustomInstantiator(f =>
+                    new UserWithEmailSeniorityAndTechnolgiesDto(
+                       f.Internet.Email(),
+                       Seniority.Unknown,
+                       new List<Technology>()));
+
+            List<UserWithEmailSeniorityAndTechnolgiesDto> listOfUserWithNoSubscribedTech
+                = usersWithNoSubscribedTechFake.Generate(5);
+
+            _userRepositoryMock.Setup(x =>
+              x.GetAllUsersWithEmailSeniorityAndTechnologiesAsync())
+                .ReturnsAsync(listOfUserWithNoSubscribedTech);
+
+            await _userAnalyzeService.LetUsersKnowAboutNewMatchingOffers();
+
+            _rabbitMessageProducerMock.Verify(x =>
+              x.SendMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task
+            Service_LetUsersKnowAboutNewMatchingOffers_Should_SendTwoMessages_WhenUsersWithoutSubscribedTechnologiesExistsAndUsersWithSubscribedTechnologiesExists()
+
+        {
+            var usersWithNoSubscribedTechFake = new Faker<UserWithEmailSeniorityAndTechnolgiesDto>()
+                 .CustomInstantiator(f =>
+                    new UserWithEmailSeniorityAndTechnolgiesDto(
+                       f.Internet.Email(),
+                       Seniority.Unknown,
+                       new List<Technology>()));
+
+            List<UserWithEmailSeniorityAndTechnolgiesDto> listOfUsers
+                = usersWithNoSubscribedTechFake.Generate(5);
+
+            listOfUsers.Add(new UserWithEmailSeniorityAndTechnolgiesDto(
+                "",
+                Seniority.Unknown,
+                new List<Technology>()
+                {
+                    new Technology()
+                }
+                ));
+
+            _userRepositoryMock.Setup(x =>
+              x.GetAllUsersWithEmailSeniorityAndTechnologiesAsync())
+                .ReturnsAsync(listOfUsers);
+
+            await _userAnalyzeService.LetUsersKnowAboutNewMatchingOffers();
+
+            _rabbitMessageProducerMock.Verify(x =>
+              x.SendMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>()), Times.Exactly(2));
+        }
+    }
+}
---
+++ UsersServiceTests/Services/UserServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersService.Interfaces.RepositoriesInterfaces;
+using UsersService.Interfaces.ServicesInterfaces;
+using UsersService.Services;
+using UsersService.Entities;
+using FluentAssertions;
+using JobOffersApiCore.Enum;
+using UsersService.Exceptions;
+
+namespace UsersServiceTests.Services
+{
+    public class UserServiceTests
+    {
+
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IClaimService> _claimServiceMock;
+        private readonly UserService _userService;
+
+        public UserServiceTests()
+        {
+            _userRepositoryMock = new();
+
+            _claimServiceMock = new();
+
+            // Setup to avoid errors (we are not testing logic of this class here)
+            _claimServiceMock.Setup(x => x.GetUserIdFromTokenClaim())
+                .Returns(Guid.Empty);
+
+            _userService = new(
+                _userRepositoryMock.Object,
+                _claimServiceMock.Object
+                );
+        }
+
+        [Fact]
+        public async Task Service_UpdateUserSeniority_Should_ThrowInvalidAccesTokenException_WhenUserFromRepositoryEqualsNull()
+        {
+            _userRepositoryMock.Setup(x => x.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync((User)null!);
+
+            await _userService.Invoking(x => x.UpdateUserSeniority(Seniority.Unknown))
+                .Should()
+                .ThrowAsync<InvalidAccesTokenException>();
+        }
+
+        [Fact]
+        public async Task Service_UpdateUserSeniority_ShouldNot_ThrowInvalidAccesTokenException_WhenUserFromRepositoryIsNotNull()
+        {
+            _userRepositoryMock.Setup(x => x.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync(new User());
+
+            await _userService.Invoking(x => x.UpdateUserSeniority(Seniority.Unknown))
+                .Should()
+                .NotThrowAsync<InvalidAccesTokenException>();
+        }
+
+        [Fact]
+        public async Task Service_UpdateUserSeniority_Should_SaveChangesIntoRepository_WhenUserFromRepositoryIsNotNull()
+        {
+            _userRepositoryMock.Setup(x => x.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync(new User());
+
+            await _userService.UpdateUserSeniority(Seniority.Unknown);
+
+            _userRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
