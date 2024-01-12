9558b874d94b42bf8e15e423a7cf02708a6eab57
Filip Krzemień <filipos2003@gmail.com>
2024-01-12 16:17:47 +0100

    test: setup integration tests base

---
+++ UsersServiceTests/Common/IntegrationTestBase.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using UsersService.DbContexts;
+
+namespace UsersServiceTests.Common
+{
+    public class IntegrationTestBase
+    {
+
+        protected readonly WebApplicationFactory<Program> _factory;
+        protected readonly HttpClient _httpClient;
+
+        public IntegrationTestBase()
+        {
+            var appFactory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        // To avoid errors due to rabbitMqConnection
+                        services.RemoveAll(typeof(IHostedService));
+
+                        services.RemoveAll(typeof(UsersDbContext));
+
+                        services.AddDbContext<UsersDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+
+                        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<UsersDbContext>));
+
+                        if (descriptor != null)
+                        {
+                            services.Remove(descriptor);
+                        }
+
+                        services.AddDbContext<UsersDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+
+                        services.AddAuthentication(TestAuthHandler.AuthenticationScheme)
+                            .AddScheme<TestAuthHandlerOptions, TestAuthHandler>(TestAuthHandler.AuthenticationScheme, options => { });
+                    });
+                });
+            _factory = appFactory;
+            _httpClient = appFactory.CreateClient();
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Test");
+        }
+
+        public void DbSeeder(Action<UsersDbContext> dbAction)
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<UsersDbContext>();
+
+                dbAction.Invoke(db);
+            }
+        }
+
+        public void ClearDatabase()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<UsersDbContext>();
+                db.Database.EnsureDeleted();
+            }
+        }
+
+        public void SetJwtUserEntitieId(Guid id)
+        {
+            _httpClient.DefaultRequestHeaders.Add("userEntiteId", id.ToString());
+        }
+
+        public UsersDbContext DbContextGetter()
+        {
+            var scope = _factory.Services.CreateScope();
+            
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<UsersDbContext>();
+
+            return db;
+        }
+    }
+}
---
+++ UsersServiceTests/Common/TestAuthHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace UsersServiceTests.Common
+{
+    public class TestAuthHandlerOptions : AuthenticationSchemeOptions
+    {
+        public string UserEntiteId { get; set; } = null!;
+    }
+
+    public class TestAuthHandler : AuthenticationHandler<TestAuthHandlerOptions>
+    {
+        public const string AuthenticationScheme = "Test";
+
+        public TestAuthHandler(
+            IOptionsMonitor<TestAuthHandlerOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+            ) 
+            : base(options, logger, encoder, clock)
+        { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new List<Claim>();
+
+            if (Context.Request.Headers.TryGetValue("userEntiteId", out var userId))
+            {
+                claims.Add(new Claim("userEntiteId" , userId!));
+            }
+
+            var identity = new ClaimsIdentity(claims, AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, AuthenticationScheme);
+
+            var result = AuthenticateResult.Success(ticket);
+
+            return Task.FromResult(result);
+        }
+    }
+}
